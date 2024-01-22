9e8ca9119a07849b91605b0d471bb8ea40b8ba9b
trippytyke <syahmi_00@hotmail.com>
2024-01-23 02:34:26 +0800

    first commit

---
+++ app/src/main/java/com/example/impactfiit/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.impactfiit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import androidx.appcompat.widget.AppCompatButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<AppCompatButton>(R.id.loginBtn)
+        val registerButton = findViewById<AppCompatButton>(R.id.registerBtn)
+
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/impactfiit/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.example.impactfiit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+
+
+    }
+}
