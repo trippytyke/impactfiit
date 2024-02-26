7be90dc96f3c931fccb7de7b256c7a93e4f7ac08
trippytyke <syahmi.ariffin@gmail.com>
2024-02-27 04:19:34 +0800

    Added activity for calorie logging and top app bar for some activities

--- app/src/main/java/com/uol/impactfiit/LogActivity.kt
+++ app/src/main/java/com/uol/impactfiit/LogActivity.kt
@@ -1,134 +1,22 @@
 package com.uol.impactfiit
 
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.bumptech.glide.Glide
-import org.json.JSONObject
+import com.google.android.material.appbar.MaterialToolbar
 
-data class Recipe(val id: Int, val name: String, val calorie: Int, val image: String)
-
-class LogActivity : AppCompatActivity() {
-    private val eatenFoods = mutableListOf<Recipe>()
-    private val recipeList = mutableListOf<Recipe>()
-
+class LogActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
 
-        val recipesRecyclerView: RecyclerView = findViewById(R.id.recyclerViewRecipes)
-        recipesRecyclerView.layoutManager = LinearLayoutManager(this)
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
 
-        val eatenFoodsRecyclerView: RecyclerView = findViewById(R.id.recyclerViewEatenFoods)
-        eatenFoodsRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        val editTextQuery: EditText = findViewById(R.id.editTextQuery)
-        val buttonSearch: Button = findViewById(R.id.buttonSearch)
-        val recipeCall = RecipeCall(this)
-
-        buttonSearch.setOnClickListener {
-            val query = editTextQuery.text.toString()
-            if (query.isNotEmpty()) {
-                recipeCall.fetchRecipes(query) { recipes ->
-                    recipeList.clear()
-                    recipeList.addAll(recipes)
-                    recipesRecyclerView.adapter?.notifyDataSetChanged()
-                }
-            } else {
-                Toast.makeText(this, "Please enter a query", Toast.LENGTH_SHORT).show()
-            }
+        topAppBar.setNavigationOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("fragShow", "log")
+            startActivity(intent)
         }
 
-        recipesRecyclerView.adapter = RecipeAdapter(recipeList) { recipe ->
-            eatenFoods.add(recipe)
-            eatenFoodsRecyclerView.adapter?.notifyDataSetChanged()
-            Toast.makeText(this, "${recipe.name} added to eaten foods", Toast.LENGTH_SHORT).show()
-        }
-
-        eatenFoodsRecyclerView.adapter = RecipeAdapter(eatenFoods) {}
-    }
-}
-
-class RecipeAdapter(private val recipeList: List<Recipe>,
-                    private val onRecipeClicked: (Recipe) -> Unit) :
-    RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
-
-    class RecipeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.findViewById(R.id.imageViewRecipe)
-        val nameTextView: TextView = view.findViewById(R.id.textViewRecipeName)
-        val caloriesTextView: TextView = view.findViewById(R.id.textViewCalories)
-        val addButton: Button = view.findViewById(R.id.buttonEaten)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_recipe, parent, false)
-        return RecipeViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
-        val recipe = recipeList[position]
-        holder.nameTextView.text = recipe.name
-        holder.caloriesTextView.text = "Calories: ${recipe.calorie}"
-        Glide.with(holder.imageView.context).load(recipe.image).into(holder.imageView)
-        holder.addButton.setOnClickListener{ onRecipeClicked(recipe)}
-    }
-
-    override fun getItemCount() = recipeList.size
-}
-class RecipeCall(val context: Context) {
-
-    val recipeList = ArrayList<Recipe>()
-
-    fun fetchRecipes(query : String, onRecipesFetched: (List<Recipe>) -> Unit) {
-        val url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=$query&maxCalories=10000"
-        val stringRequest = object : StringRequest(
-            Method.GET, url,
-            Response.Listener { response ->
-                val jsonObject = JSONObject(response)
-                val results = jsonObject.getJSONArray("results")
-                for (i in 0 until results.length()) {
-                    val result = results.getJSONObject(i)
-                    val id = result.getInt("id")
-                    val title = result.getString("title")
-                    val image = result.getString("image")
-                    val nutrition = result.optJSONObject("nutrition")
-                    val calories = if (nutrition != null) {
-                        val nutrients = nutrition.getJSONArray("nutrients")
-                        nutrients.getJSONObject(0).getInt("amount")
-                    } else {
-                        0
-                    }
-                    recipeList.add(Recipe(id, title, calories, image))
-                }
-                onRecipesFetched(recipeList)
-            },
-            Response.ErrorListener { error ->
-                Log.e("RecipeCall", "Failed to fetch recipes: $error")
-                Toast.makeText(context, "Error fetching recipes", Toast.LENGTH_SHORT).show()
-            }) {
-            override fun getHeaders(): Map<String, String> {
-                val headers = HashMap<String, String>()
-                headers["X-RapidAPI-Key"] = "462895ac8emshb93a880bc9aac7fp1ca9dfjsn3a8ff0c341ee"
-                headers["X-RapidAPI-Host"] = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-                return headers
-            }
-        }
-
-        Volley.newRequestQueue(context).add(stringRequest)
     }
 }
--- app/src/main/java/com/uol/impactfiit/LogFragment.kt
+++ app/src/main/java/com/uol/impactfiit/LogFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 
 class LogFragment:Fragment(R.layout.fragment_log) {
@@ -16,12 +17,16 @@ class LogFragment:Fragment(R.layout.fragment_log) {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_log, container, false)
 
-        val buttonNavigate: Button = view.findViewById(R.id.btnNavigate)
-        buttonNavigate.setOnClickListener {
+        val buttonExplore: ImageButton = view.findViewById(R.id.btnExplore)
+        val buttonLog: ImageButton = view.findViewById(R.id.btnLog)
+        buttonExplore.setOnClickListener {
+            val intent = Intent(activity, RecipeActivity::class.java)
+            startActivity(intent)
+        }
+        buttonLog.setOnClickListener {
             val intent = Intent(activity, LogActivity::class.java)
             startActivity(intent)
         }
-
         return view
     }
 }
